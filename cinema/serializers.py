8ae63528c4f9277eb7accc76affb6ff219be3864
lassisterv16 <lassisterv16@gmail.com>
2026-07-30 21:50:26 +0200

    fix flake8 errors

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -7,21 +7,21 @@ class CinemaHallSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = CinemaHall
-        fields = ["id", "name", "rows", "seats_in_row", "capacity",]
+        fields = ["id", "name", "rows", "seats_in_row", "capacity", ]
 
 
 class GenreSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Genre
-        fields = ["id", "name",]
+        fields = ["id", "name", ]
 
 
 class ActorSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Actor
-        fields = ["id", "first_name", "last_name", "full_name",]
+        fields = ["id", "first_name", "last_name", "full_name", ]
 
 
 class MovieSerializer(serializers.ModelSerializer):
@@ -51,7 +51,7 @@ class MovieSessionSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = MovieSession
-        fields = ["id", "show_time", "movie", "cinema_hall",]
+        fields = ["id", "show_time", "movie", "cinema_hall", ]
 
 
 class MovieSessionListSerializer(MovieSessionSerializer):
